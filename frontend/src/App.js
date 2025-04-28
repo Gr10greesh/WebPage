4fdc486c3c5683c4c91e62b8365612e7b39b2196
Gr10greesh <greeshdahal432@gmail.com>
2025-04-28 19:14:18 +0545

    Changed footer section hide in my Login page, dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import LoginSignup from "./Pages/LoginSignup";
@@ -10,40 +10,61 @@ import ProductList from "./Pages/ProductList";
 import ProtectedRoute from "./Components/Auth/ProtectedRoute";
 import DashboardHome from './Components/Dashboard/DashboardHome';
 import OrderHistory from './Components/Dashboard/OrderHistory';
-import Dashboard from './Components/Dashboard/Dashboard'
+import Dashboard from './Components/Dashboard/Dashboard';
 import Profile from './Components/Dashboard/Profile';
 import Checkout from "./Components/Checkout/Checkout";
 import PaymentCallback from "./Components/PaymentCallback/PaymentCallback";
 import OrderSuccess from "./Components/OrderSuccess/OrderSuccess";
+import Chatbot from "./Components/Chatbot/Chatbot";
 
 function App() {
+  const location = useLocation(); // 👈 Get the current URL path
+
+  // List of paths where Footer should not appear
+  const hideFooterRoutes = [
+    '/login', 
+    '/dashboard', 
+    '/dashboard/orders', 
+    '/dashboard/profile'
+  ];
+
+  // Check if we should hide Footer
+  const shouldHideFooter = hideFooterRoutes.some(path => location.pathname.startsWith(path));
+
   return (
     <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<ShopCategory category="all" />} />
-          <Route path="/giftcard" element={<ShopCategory category="giftcard" />} />
-          <Route path="/mobilegames" element={<ShopCategory category="mobilegames" />} />
-          <Route path="/freefire" element={<ShopCategory category="freefire" />} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/products/:productId" element={<Product />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<LoginSignup />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/payment-callback" element={<PaymentCallback />} />
-          <Route path="/order-success" element={<OrderSuccess />} />
-          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ShopCategory category="all" />} />
+        <Route path="/giftcard" element={<ShopCategory category="giftcard" />} />
+        <Route path="/mobilegames" element={<ShopCategory category="mobilegames" />} />
+        <Route path="/freefire" element={<ShopCategory category="freefire" />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/products/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignup />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/payment-callback" element={<PaymentCallback />} />
+        <Route path="/order-success" element={<OrderSuccess />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>}>
           <Route index element={<DashboardHome />} />
           <Route path="profile" element={<Profile />} />
           <Route path="orders" element={<OrderHistory />} />
-          </Route>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+        </Route>
+      </Routes>
+
+      {/* Only show Footer if shouldHideFooter is false */}
+      {!shouldHideFooter && <Footer />}
     </div>
   );
 }
 
-export default App;
+export default function AppWrapper() {
+  return (
+    <BrowserRouter>
+      <App />
+      <Chatbot />
+    </BrowserRouter>
+  );
+}
