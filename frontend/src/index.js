3c20fa405f9ca10e2409ec0fd41e7a7c7c97c00c
Gr10greesh <greeshdahal432@gmail.com>
2025-04-30 12:38:13 +0545

    Added Toastify in index.js code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,17 +4,15 @@ import './index.css';
 import App from './App';
 import ShopContextProvider from './Context/ShopContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-// import { ToastContainer } from 'react-toastify'; // Import ToastContainer
-
+import { ToastContainer } from 'react-toastify';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
   <ShopContextProvider>
-    <App />
-    {/* <ToastContainer position="top-right"
+        <App />
+    <ToastContainer position="top-right"
       draggable
-    
-    /> Add ToastContainer here */}
+    />
   </ShopContextProvider>
   </GoogleOAuthProvider>
 );
