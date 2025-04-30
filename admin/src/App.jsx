fc48fabbb95c30e515520857f177ad707992805e
Gr10greesh <greeshdahal432@gmail.com>
2025-04-30 12:30:37 +0545

    Route path added for admin panel login page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Admin from './Pages/Admin/Admin'
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Admin from './Pages/Admin/Admin';
+import AdminLogin from './Pages/Admin/AdminLogin';
+import AdminProtectedRoute from './Components/Auth/AdminProtectedRoute';
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Admin/>
-    </div>
-  )
-}
+      <Navbar />
+      <Routes>
+        {/* Redirect "/" to /admin-login */}
+        <Route path="/" element={<Navigate to="/admin-login" replace />} />
 
-export default App
+        {/* Admin login page */}
+        <Route path="/admin-login" element={<AdminLogin />} />
+
+        {/* Admin dashboard with nested routes */}
+        <Route 
+          path="/admin/*" 
+          element={
+            <AdminProtectedRoute>
+              <Admin />
+            </AdminProtectedRoute>
+          } 
+        />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
