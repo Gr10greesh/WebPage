282475797639effb965a7aca33111eff83a66835
Gr10greesh <greeshdahal432@gmail.com>
2025-04-30 12:31:56 +0545

    Toastify wrapped inside main.jsx code root

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-
+import { ToastContainer } from 'react-toastify';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer position="top-right" autoClose={3000} pauseOnHover />
     </BrowserRouter>
   </StrictMode>
 );
